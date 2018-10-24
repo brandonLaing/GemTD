aee9da4a706ca50fd35396ca7d1bf1e10026d926
Brandon Laing <brandon.robert.laing@gmail.com>
2018-10-24 18:25:20 -0500

    finished up camera movement for now

--- Assets/Scripts/Camera/CameraDrag.cs
+++ Assets/Scripts/Camera/CameraDrag.cs
@@ -1,113 +1,131 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CameraDrag : MonoBehaviour
 {
-  [Header("others way")]
+  [Header("Drag Variables")]
   public float dragSpeed = 2;
-  public Vector3 dragOrigin;
-  public Vector2 mousePosition;
+  private Vector2 dragMouseOrigin;
+  private Vector3 dragCameraOrigin;
+  private Vector2 mousePosition;
 
-  [Space()]
-  public float right;
-  public float left;
-  public float top;
-  public float bottom;
+  [Header("Mouse Boundaries")]
+  private float rightOfScreen;
+  private float leftOfScreen;
+  private float topOfScreen;
+  private float bottomOfScreen;
 
-  public bool cameraDragging = true;
+  [Header("Lets know if were dragging")]
+  public bool draggingCamera;
 
   private void Start()
   {
-    right = Screen.width;
-    left = 0;
-    top = Screen.height;
-    bottom = 0;
+    rightOfScreen = Screen.width;
+    leftOfScreen = 0;
+    topOfScreen = Screen.height;
+    bottomOfScreen = 0;
   }
 
   void Update ()
   {
-    mousePosition = Input.mousePosition;
     //OthersWay2();
-    MoveMouse();
+    FindCameraDrag();
   }
 
-  private void MoveMouse()
+  // We are doing all of our movement based off the origin
+  // the idea is they move in relation to that spot so if they move right 3 units then they move back left 3 units they will be back at that point
+
+  private void FindCameraDrag()
   {
-    // when the player presses down the grip get that origin
+    mousePosition = Input.mousePosition;
+
+    // get the origin of the mouse and camera and lock the camera to the screen then return so were not moving on our first frame
     if (Input.GetMouseButtonDown(2))
     {
-      dragOrigin = Input.mousePosition;
+      dragMouseOrigin = Input.mousePosition;
+      dragCameraOrigin = transform.position;
+      Cursor.lockState = CursorLockMode.Confined;
       return;
     }
 
-    // if were currently dragging
+    // while were dragging set the bool to true
     if (Input.GetMouseButton(2))
     {
-      // do the movement of the x axis
-      if (mousePosition.x > left && mousePosition.x < right)
-      {
-        Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
-        Vector3 move = new Vector3(pos.x * dragSpeed, 0, 0);
+      draggingCamera = true;
 
-        transform.position += move;
+      // find the movement on the axis'. Make sure its within bounds of the screen
+      if (mousePosition.x > leftOfScreen && mousePosition.x < rightOfScreen &&
+          mousePosition.y > bottomOfScreen && mousePosition.y < topOfScreen)
+      {
+        Vector3 changeInMousePosition = new Vector3();
+
+        changeInMousePosition = Camera.main.ScreenToViewportPoint(mousePosition - dragMouseOrigin);
+
+        // multiply each variable by the drag speed
+        Vector3 amountMovedThisFrame = new Vector3(-changeInMousePosition.x * dragSpeed, 0, -changeInMousePosition.y * dragSpeed);
+
+        // then add the camera origin by the ammount moved this frame
+        transform.position = dragCameraOrigin + amountMovedThisFrame;
+
+         
       }
     }
-    // We are doing all of our movement based off the origin
-    // the idea is they move in relation to that spot so if they move right 3 units then they move back left 3 units they will be back at that point
-  }
-
-  private void OthersWay()
-  {
-    if (Input.GetMouseButtonDown(0))
+    else
     {
-      dragOrigin = Input.mousePosition;
-      return;
-    }
-
-    if (!Input.GetMouseButtonUp(0)) return;
-
-    if (Input.GetMouseButton(0))
-    {
-      Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
-      Vector3 move = new Vector3(pos.x * dragSpeed, 0, pos.y * dragSpeed);
-      transform.Translate(move, Space.World);
-
+      draggingCamera = false;
     }
   }
 
-  private void OthersWay2()
-  {
-    Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+  //private void OthersWay()
+  //{
+  //  if (Input.GetMouseButtonDown(0))
+  //  {
+  //    dragMouseOrigin = Input.mousePosition;
+  //    return;
+  //  }
 
-    float left = Screen.width * 0.2F;
-    float right = Screen.width - (Screen.width * 0.2F);
+  //  if (!Input.GetMouseButtonUp(0)) return;
 
-    if (mousePosition.x < left)
-    {
-      cameraDragging = true;
-    }
+  //  if (Input.GetMouseButton(0))
+  //  {
+  //    Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragMouseOrigin);
+  //    Vector3 move = new Vector3(pos.x * dragSpeed, 0, pos.y * dragSpeed);
+  //    transform.Translate(move, Space.World);
 
-    else if (mousePosition.x > right)
-    {
-      cameraDragging = true;
-    }
+  //  }
+  //}
 
-    if (cameraDragging)
-    {
-      if (Input.GetMouseButtonDown(2))
-      {
-        dragOrigin = Input.mousePosition;
-        return;
-      }
+  //private void OthersWay2()
+  //{
+  //  Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
 
-      if (!Input.GetMouseButton(2)) return;
+  //  float left = Screen.width * 0.2F;
+  //  float right = Screen.width - (Screen.width * 0.2F);
 
-      Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
-      Vector3 move = new Vector3(pos.x * dragSpeed, 0, 0);
+  //  if (mousePosition.x < left)
+  //  {
+  //    draggingCamera = true;
+  //  }
 
-      transform.Translate(move, Space.World);
-    }
+  //  else if (mousePosition.x > right)
+  //  {
+  //    draggingCamera = true;
+  //  }
 
-  }
+  //  if (draggingCamera)
+  //  {
+  //    if (Input.GetMouseButtonDown(2))
+  //    {
+  //      dragMouseOrigin = Input.mousePosition;
+  //      return;
+  //    }
+
+  //    if (!Input.GetMouseButton(2)) return;
+
+  //    Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragMouseOrigin);
+  //    Vector3 move = new Vector3(pos.x * dragSpeed, 0, 0);
+
+  //    transform.Translate(move, Space.World);
+  //  }
+
+  //}
 }
