678e125be03e728c7d55ca28c32be584aaae1720
Brandon Laing <blaing2@stumail.jccc.edu>
2018-10-25 13:49:13 -0500

    started work on tower placeing

---
+++ Assets/Scripts/Camera/TowerPlacement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerPlacement : MonoBehaviour
+{
+  public LayerMask tileLayer;
+  public float raycastRange = 20F;
+  public Camera myCamera;
+
+  public GameObject[] towers;
+
+  private void Update()
+  {
+    if (Input.GetMouseButtonDown(1))
+    {
+      RaycastHit hit;
+      Ray ray = myCamera.ScreenPointToRay(Input.mousePosition);
+
+      if (Physics.Raycast(ray, out hit, raycastRange, tileLayer))
+      {
+        if (hit.transform.GetComponentInParent<TileInformation>())
+        {
+          TileInformation tileInfo = hit.transform.GetComponentInParent<TileInformation>();
+          tileInfo.BuildTower(RandomTower());
+
+        }
+      }
+    }
+  }
+
+  private GameObject RandomTower()
+  {
+    int randomTower = Random.Range(0, towers.Length);
+    return towers[randomTower];
+  }
+}
